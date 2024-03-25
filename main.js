5340d208a71b59f8f1bc2e5a5f30216b3e606e77
julia noda <111542735+juhnoda@users.noreply.github.com>
2024-03-25 08:49:24 -0300

    Grupo_4

--- main.js
+++ main.js
@@ -33,6 +33,11 @@ function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos/60);
+    
+    return segundos;
+
+}
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
@@ -42,4 +47,4 @@ function calculaTempo(tempoObjetivo) {
 
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
     
-}
+
