ac84acbce9dd1df8fbbefbf486851c29f27c2629
julia noda <111542735+juhnoda@users.noreply.github.com>
2024-08-27 09:43:46 -0300

    aula 19

--- main.js
+++ main.js
@@ -10,10 +10,13 @@ function diminuiTamanho(){
     if (tamanhoSenha > 1)
  tamanhoSenha = tamanhoSenha - 1;
  numeroSenha.textContent = tamanhoSenha
+ numeroSenha.textContent = tamanhoSenha;
+ geraSenha();
 }
 function aumentaTamanho() {
     if (tamanhoSenha < 20) {
     tamanhoSenha = tamanhoSenha + 1;
+    geraSenha();
     }
     numeroSenha.textContent = tamanhoSenha;
     }
@@ -23,10 +26,45 @@ botoes[1].onclick = aumentaTamanho;
 // código omitido
 const campoSenha = document.querySelector("#campo-senha");
 const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVXYWZ";
-function geraSenha(){
+const numeros = "0123456789";
+const simbolos = "!@%*?";
+const botoes = document.querySelectorAll(".parametro-senha__botao");    
+const campoSenha = document.querySelector("campo-senha");
+const checkbox = document.querySelectorAll(".checkbox");
+{
+function geraSenha()
+    let senha = " ";
     for (let i = 0; i < tamanhoSenha;i++)
     let numeroAleatorio = Math.random()*letrasMaiusculas.length;
     numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + letrasMaiusculas[numeroAleatorio];
        console.log(letrasMaiusculas[numeroAleatorio]);
+       let alfabeto = "";
+       campoSenha.value= senha;
+        if (checkbox[0].checked){
+       alfabeto = alfabeto + letrasMaiusculas;
+      }
+      if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+        }
+        if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+        }
+        if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+        }
+         console.log(alfabeto);
+         let senha = "";
+         for (let i = 0; i < tamanhoSenha;i++)
+         let numeroAleatorio = Math.random()*alfabeto.length;
+         numeroAleatorio = Math.floor(numeroAleatorio);
+         senha = senha + alfabeto[numeroAleatorio];
+     const checkbox = document.querySelectorAll(".checkbox");
+     for (i=0; i < checkbox.length;i++){
+        checkbox[i].onclick = geraSenha;
+    console.log(checkbox[0].checked); }
+    const forcaSenha = document.querySelector('.forca');
+    function classificaSenha(){
+    forcaSenha.classList.add("forte")
+    }
 }
-campoSenha.value= letrasMaiusculas;
